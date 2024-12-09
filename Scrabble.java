2d80c1e55ee896b81d848d9d9277f13e75532493
tidharhayute-runi <tidharhayute@gmail.com>
2024-12-09 20:58:24 +0200

    Submit

--- Scrabble.java
+++ Scrabble.java
@@ -118,7 +118,13 @@ public class Scrabble {
 				}
 				
 			} else System.out.println("Invalid word. Try again.");
-		}	
+		}
+		
+		if (hand.length() == 0) {
+	        System.out.println("Ran out of letters. Total score: " + score + " points");
+		} else {
+			System.out.println("End of hand. Total score: " + score + " points");
+		}
 	}
 
 
