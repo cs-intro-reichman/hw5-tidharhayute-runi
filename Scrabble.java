96068cad4d855a12677d34b549e495bf452717b4
tidharhayute-runi <tidharhayute@gmail.com>
2024-12-09 17:43:00 +0200

    Submit

--- Scrabble.java
+++ Scrabble.java
@@ -102,7 +102,7 @@ public class Scrabble {
 			// non-whitespace characters. Whitespace is either space characters, or  
 			// end-of-line characters.
 			String input = in.readString();
-			if (input.equals('.')) break;
+			if (input.equals(".")) break;
 			
 			if (MyString.subsetOf(input, hand)) {
 				if (isWordInDictionary(input)) {
